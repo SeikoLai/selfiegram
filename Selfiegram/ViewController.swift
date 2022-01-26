1c7b9850b843b77798d45e4085d0834cc90fe6a1
Sam Lai <sam.lai@udnshopping.com>
2022-01-26 10:17:36 +0800

    Initial Commit

---
+++ Selfiegram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Selfiegram
+//
+//  Created by Sam Lai on 2022/1/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
